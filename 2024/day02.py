03c3076a98222ba3dac483a327976333a3e78b5e
Jules <julesbodin04@gmail.com>
2024-12-02 06:39:54 +0100

    Day 2 year 2024

---
+++ 2024/day02.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import sys
+import aocparser as ap
+
+
+def safe(tab):
+    is_sorted = tab == sorted(tab) or tab == sorted(tab, reverse=True)
+    is_safe = True
+    for i in range(len(tab) - 1):
+        diff = abs(tab[i] - tab[i + 1])
+        if not 1 <= diff <= 3:
+            is_safe = False
+            break
+    return is_sorted and is_safe
+
+
+def part1(filename):
+    result = 0
+    with open(filename, "r") as file:
+        lines = ap.file2lines(file)
+        for line in lines:
+            stripped = list(map(int, line.split(" ")))
+            if safe(stripped):
+                result += 1
+        file.close()
+    return result
+
+
+def part2(filename):
+    result = 0
+    with open(filename, "r") as file:
+        lines = ap.file2lines(file)
+        for line in lines:
+            stripped = list(map(int, line.split(" ")))
+
+            is_good = False
+            for j in range(len(stripped)):
+                tab = stripped[:j] + stripped[j + 1 :]
+                if safe(tab):
+                    is_good = True
+
+            if is_good:
+                result += 1
+        file.close()
+    return result
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Input missing !")
+        sys.exit(1)
+
+    filename = sys.argv[1]
+
+    p1 = part1(filename)
+    print(p1)
+    p2 = part2(filename)
+    print(p2)
+
+
+if __name__ == "__main__":
+    main()
